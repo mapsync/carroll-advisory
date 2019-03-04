9d1c8d9c2d3d64c4309c865eecc4bb45e42ce589
Gitea <gitea@fake.local>
2019-03-04 12:16:04 -0500

    map fix

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout'
 
 export default class Index extends React.Component {
   componentDidMount() {
-    document.getElementById("map").src = "https://geosync.cloud/maps/e2080ba6-7192-4d74-afdb-390d7b1db825" + this.props.location.search;
+    document.getElementById("map").src = "https://geosync.cloud/maps/0a480ef3-ec0d-4a5f-adf3-c7e2d8f608ba" + this.props.location.search;
   }
   
   render() {
