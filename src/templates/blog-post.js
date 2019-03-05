a29c10b533cffca9af9fa049818e802df59dcacd
Gitea <gitea@fake.local>
2019-03-05 09:07:14 -0500

    domain fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -49,7 +49,7 @@ export const BlogPostTemplate = ({
       </div>
       <div className="footer">
         <div className="d-inline-block float-right">
-          <a className="btn btn-sm btn-link tooltip tooltip-left" data-tooltip="Settings" rel="noopener noreferrer" href="https://woodcreek.geosync.cloud/admin" target="_blank">
+          <a className="btn btn-sm btn-link tooltip tooltip-left" data-tooltip="Settings" rel="noopener noreferrer" href="https://carrollwater.geosync.cloud/admin" target="_blank">
             <Icon.Settings size={16}/>
           </a>
         </div>
@@ -70,9 +70,9 @@ BlogPostTemplate.propTypes = {
 const BlogPost = ({ data }) => {
   const { markdownRemark: post } = data
 
-  var re = new RegExp("https://geosync.cloud/maps/e2080ba6-7192-4d74-afdb-390d7b1db825", "g");
+  var re = new RegExp("https://geosync.cloud/maps/0a480ef3-ec0d-4a5f-adf3-c7e2d8f608ba", "g");
   post.html = post.html.replace(re, "/map");
-  re = new RegExp("https://woodcreek.geosync.cloud/map", "g");
+  re = new RegExp("https://carrollwater.geosync.cloud/map", "g");
   post.html = post.html.replace(re, "/map");
 
   return (
