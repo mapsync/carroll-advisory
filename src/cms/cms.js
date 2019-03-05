52a2af80e08d34b14bf7d6f74ed095d3ae4cb908
Gitea <gitea@fake.local>
2019-03-05 13:41:30 -0500

    domain fix

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -38,7 +38,7 @@ var NotificationControl = class Control extends React.Component {
       disabled: true
     });
     var link = document.getElementById("link-field-" + (id - 1)).value;
-    if (link.startsWith("https://carrollwater.geosync.cloud")) {
+    if (link.startsWith("https://carrollcountywater.geosync.cloud")) {
       var url = new URL(link);
       link = "https://zealous-cori-f939d0.netlify.com" + url.pathname + url.search
     }
@@ -47,7 +47,7 @@ var NotificationControl = class Control extends React.Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": "notification",
-        "account": "carrollwater",
+        "account": "carrollcountywater",
         "title": document.getElementById("title-field-" + (id - 5)).value,
         "priority": document.getElementById("priority-field-" + (id - 4)).getAttribute("aria-checked"),
         "sound": document.getElementById("sound-field-" + (id - 3)).getAttribute("aria-checked"),
